5d157318b70d8bc280eacc00cbb025d6ae60b784
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2024-06-05 12:39:54 +0000

    background

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -14,17 +14,7 @@ export class Game extends Scene
     create ()
     {
         this.camera = this.cameras.main;
-        this.camera.setBackgroundColor(0x00ff00);
-
-        this.background = this.add.image(512, 384, 'background');
-        this.background.setAlpha(0.5);
-
-        this.msg_text = this.add.text(512, 384, 'Hello, World!', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        });
-        this.msg_text.setOrigin(0.5);
+        this.camera.setBackgroundColor(0x88A4A8);
 
         this.input.once('pointerdown', () => {
 
